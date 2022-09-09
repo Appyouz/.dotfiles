dea845472e152aca2f4ddc71b839892562fcb552
Appyouz <91145436+Appyouz@users.noreply.github.com>
2022-09-09 15:27:37 +0545

    first commit

---
+++ .config/suckless/dmenu/config.h
@@ -0,0 +1,36 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"JetBrainsMono Medium Nerd Font:size=12"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+//static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	//[SchemeNorm] = { "#bbbbbb", "#222222" },
+	//[SchemeSel] = { "#eeeeee", "#005577" },
+	//[SchemeOut] = { "#000000", "#00ffff" },
+//};
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#bd93f9", "#282a36" },
+	[SchemeSel] = { "#50fa7b", "#282a36" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+};
+
+
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 10;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static const unsigned int border_width = 5;
