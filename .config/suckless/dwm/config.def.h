1b78b0ed42543d15a30637a86a2bdee39c3e74e9
Appyouz <91145436+Appyouz@users.noreply.github.com>
2022-09-11 17:37:51 +0545

    idk

--- .config/suckless/dwm/config.def.h
+++ .config/suckless/dwm/config.def.h
@@ -42,7 +42,7 @@ static const char *voldowncmd[] = { "pactl", "set-sink-volume", "0", "-5%", NULL
 /* tagging */
 //static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 /* static const char *tags[] = {" ","","","","","","","",""}; */
-static const char *tags[] = {"",""," ","","","",""}; 
+static const char *tags[] = {"","","","","","",""}; 
 
 static const Rule rules[] = {
 	/* xprop(1):
