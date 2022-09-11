cc1c552004435b2f4d4ef5ba273262a64710bd82
Appyouz <91145436+Appyouz@users.noreply.github.com>
2022-09-11 14:59:11 +0545

    tags rule added

--- .config/suckless/dwm/config.h
+++ .config/suckless/dwm/config.h
@@ -41,7 +41,8 @@ static const char *voldowncmd[] = { "pactl", "set-sink-volume", "0", "-5%", NULL
 
 /* tagging */
 //static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-static const char *tags[] = {" ","","","","","","","",""};
+/* static const char *tags[] = {" ","","","","","","","",""}; */
+static const char *tags[] = {"",""," ","","","",""}; 
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -50,7 +51,9 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "firefox",  NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "Alacritty",NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "sublime-text",   NULL,       NULL,       1 << 2,       0,           -1 },
 };
 
 /* layout(s) */
