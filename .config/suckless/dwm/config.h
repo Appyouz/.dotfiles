d0143588b1c5914b7d8715db8809b1a117dd5545
appyouz <Dikpalkhatri7@gmail.com>
2022-10-06 22:55:36 +0545

    added tag dwm

--- .config/suckless/dwm/config.h
+++ .config/suckless/dwm/config.h
@@ -49,7 +49,7 @@ static const char *pkill[] = {"pkill", "dwm", NULL};
 
 /* tagging */
 // static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-static const char *tags[] = {"","","","","",""}; 
+static const char *tags[] = {"","","","","","","",""}; 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
@@ -58,6 +58,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "subl",     NULL,       NULL,       1 << 1,       0,           -1 },
 };
 
 /* layout(s) */
