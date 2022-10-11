40e3f0800b2e77de2f711f5ff2779438084b4644
Appyouz <Dikpalkhatri7@gmail.com>
2022-10-11 23:17:49 +0545

    documents arranged

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -45,7 +45,7 @@ return packer.startup(function(use)
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
   use 'windwp/nvim-autopairs' -- An autopair
-  use 'windwp/nvim-ts-autotag'
+  -- use 'windwp/nvim-ts-autotag'
   use "lukas-reineke/indent-blankline.nvim"
   use({ "akinsho/bufferline.nvim", commit = "c78b3ecf9539a719828bca82fc7ddb9b3ba0c353" })
   use  'numToStr/Comment.nvim'
