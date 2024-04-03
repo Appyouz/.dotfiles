dd1b2031f3d7fef2d11233279351c0ac0e13aec1
appyouz <dikpalkhatri7@gmail.com>
2024-04-03 16:44:39 +0545

    New installation method for neorg

--- .config/nvim/lua/user/plugins/colorschemes.lua
+++ .config/nvim/lua/user/plugins/colorschemes.lua
@@ -10,7 +10,12 @@ return {
 			vim.g.gruvbox_material_better_performance = 1
 			-- vim.g.gruvbox_material_transparent_background = 2
 		end,
-
 	},
-
+	{
+		"morhetz/gruvbox",
+		config = function()
+			-- vim.cmd.colorscheme("gruvbox")
+      vim.g.gruvbox_contrast_dark = "soft"
+		end,
+	},
 }
--- .config/nvim/lua/user/plugins/neorg.lua
+++ .config/nvim/lua/user/plugins/neorg.lua
@@ -1,24 +1,31 @@
 return {
-  "nvim-neorg/neorg",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  -- build = ":Neorg sync-parsers",
-  -- tag = "*",
-  lazy = true, -- enable lazy load
-  ft = "norg", -- lazy load on file type
-  cmd = "Neorg", -- lazy load on command
-  config = function()
-    require("neorg").setup {
-      load = {
-        ["core.defaults"] = {}, -- Loads default behaviour
-        ["core.concealer"] = {}, -- Adds pretty icons to your documents
-        ["core.dirman"] = { -- Manages Neorg workspaces
-          config = {
-            workspaces = {
-              notes = "~/notes",
+  {
+    "vhyrro/luarocks.nvim",
+    priority = 1000,
+    config = true,
+  },
+  {
+    "nvim-neorg/neorg",
+    dependencies = { "luarocks.nvim" },
+    lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+    version = "*", -- Pin Neorg to the latest stable release
+    -- config = true,
+    config = function()
+      require("neorg").setup({
+        load = {
+          ["core.defaults"] = {}, -- Loads default behaviour
+          -- ["core.keybinds"] = {},
+          -- ["core.qol.todo_items"] = {},
+          ["core.concealer"] = {}, -- Adds pretty icons to your documents
+          ["core.dirman"] = { -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                notes = "~/notes",
+              },
             },
           },
         },
-      },
-    }
-  end,
+      })
+    end,
+  },
 }
