bf1cdbb9e8b2a427104741ca3cc6d1016cae4ab0
appyouz <dikpalkhatri7@gmail.com>
2025-04-30 09:53:11 +0545

    NO idea what changes i did

--- .config/nvim/lua/plugins/autopair.lua
+++ .config/nvim/lua/plugins/autopair.lua
@@ -6,20 +6,22 @@ return {
 		-- use opts = {} for passing setup options
 		-- this is equivalent to setup({}) function
 	},
-	"windwp/nvim-ts-autotag",
-	opt = {
-		-- Defaults
-		enable_close = true, -- Auto close tags
-		enable_rename = true, -- Auto rename pairs of tags
-		enable_close_on_slash = false, -- Auto close on trailing </
-		map_cr = true,
-	},
-	-- Also override individual filetype configs, these take priority.
-	-- Empty by default, useful if one of the "opts" global settings
-	-- doesn't work well in a specific filetype
-	per_filetype = {
-		["html"] = {
-			enable_close = false,
+	{
+		"windwp/nvim-ts-autotag",
+		opt = {
+			-- Defaults
+			enable_close = true, -- Auto close tags
+			enable_rename = true, -- Auto rename pairs of tags
+			enable_close_on_slash = false, -- Auto close on trailing </
+			map_cr = true,
+		},
+		-- Also override individual filetype configs, these take priority.
+		-- Empty by default, useful if one of the "opts" global settings
+		-- doesn't work well in a specific filetype
+		per_filetype = {
+			["html"] = {
+				enable_close = false,
+			},
 		},
 	},
 }
