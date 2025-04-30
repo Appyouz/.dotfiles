a407b9a78b24fdf48376cd33a48c036c5b6ea4ed
appyouz <dikpalkhatri7@gmail.com>
2025-04-30 15:19:46 +0545

    Refactor: removed lspsaga cause stutter when scrolling

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -6,10 +6,8 @@ return {
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
 		{ "j-hui/fidget.nvim", opts = {} },
 		"ibhagwan/fzf-lua",
-		"glepnir/lspsaga.nvim",
 	},
 	config = function()
-		-- Auto-command for showing diagnostics on cursor hold
 		vim.api.nvim_create_autocmd("CursorHold", {
 			callback = function()
 				local opts = {
@@ -24,87 +22,98 @@ return {
 			end,
 		})
 
-		-- LSP flags
+		-- Define lsp_flags
 		local lsp_flags = {
+			-- This is the default in Nvim v0.7+
 			debounce_text_changes = 150,
 		}
 
-		-- On_attach function with key mappings
+		-- Define on_attach function
 		local on_attach = function(client, bufnr)
 			local map = function(keys, func, desc, mode)
 				mode = mode or "n"
 				vim.keymap.set(mode, keys, func, { buffer = bufnr, desc = "LSP: " .. desc })
 			end
 
-			-- Lspsaga mappings (preserving old config features)
-			map("gd", "<Cmd>Lspsaga finder<CR>", "[G]oto [D]efinition and References")
-			map("K", "<Cmd>Lspsaga hover_doc<CR>", "Hover Documentation")
-			map("<leader>ca", "<Cmd>Lspsaga code_action<CR>", "[C]ode [A]ction", { "n", "x" })
-			map("[d", "<Cmd>Lspsaga diagnostic_jump_prev<CR>", "Previous Diagnostic")
-			map("]d", "<Cmd>Lspsaga diagnostic_jump_next<CR>", "Next Diagnostic")
-			map("<leader>rn", "<Cmd>Lspsaga rename<CR>", "[R]ename")
-			map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-			map("<C-k>", "<Cmd>Lspsaga signature_help<CR>", "Signature Help", "i")
+			-- Common LSP key mappings (using built-in Neovim LSP functions)
+			map("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+			map("gr", vim.lsp.buf.references, "[G]oto [R]eferences")
+			map("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+			map("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
+			map("<leader>ds", vim.lsp.buf.document_symbol, "[D]ocument [S]ymbols")
+			map("<leader>ws", vim.lsp.buf.workspace_symbol, "[W]orkspace [S]ymbols")
+			map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+			map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
+			map("<leader>cl", vim.lsp.buf.declaration, "[C]ode [L]ocation")
+			map("<leader>cf", vim.lsp.buf.format, "[C]ode [F]ormat")
 
-			-- Fzf-lua mappings from new config
+			-- Key mappings using fzf-lua (keeping your original mappings)
+			map("gd", require("fzf-lua").lsp_definitions, "[G]oto [D]efinition")
 			map("gr", require("fzf-lua").lsp_references, "[G]oto [R]eferences")
 			map("gI", require("fzf-lua").lsp_implementations, "[G]oto [I]mplementation")
 			map("<leader>D", require("fzf-lua").lsp_typedefs, "Type [D]efinition")
 			map("<leader>ds", require("fzf-lua").lsp_document_symbols, "[D]ocument [S]ymbols")
 			map("<leader>ws", require("fzf-lua").lsp_live_workspace_symbols, "[W]orkspace [S]ymbols")
 
-			-- Workspace folder mappings
-			map("<space>wa", vim.lsp.buf.add_workspace_folder, "Add Workspace Folder")
-			map("<space>wr", vim.lsp.buf.remove_workspace_folder, "Remove Workspace Folder")
-			map("<space>wl", function()
-				print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-			end, "List Workspace Folders")
+			local function client_supports_method(client, method, bufnr)
+				if vim.fn.has("nvim-0.11") == 1 then
+					return client:supports_method(method, bufnr)
+				else
+					return client.supports_method(method, { bufnr = bufnr })
+				end
+			end
 
-			-- Formatting
-			map("<leader>cf", vim.lsp.buf.format, "[C]ode [F]ormat")
-
-			-- Document highlighting
-			if client and client.server_capabilities.documentHighlightProvider then
-				local highlight_augroup = vim.api.nvim_create_augroup("lsp-highlight", { clear = false })
+			if
+				client
+				and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, bufnr)
+			then
+				local highlight_augroup = vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
 				vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
 					buffer = bufnr,
 					group = highlight_augroup,
 					callback = vim.lsp.buf.document_highlight,
 				})
+
 				vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
 					buffer = bufnr,
 					group = highlight_augroup,
 					callback = vim.lsp.buf.clear_references,
 				})
+
 				vim.api.nvim_create_autocmd("LspDetach", {
-					group = vim.api.nvim_create_augroup("lsp-detach", { clear = true }),
+					group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
 					buffer = bufnr,
-					callback = function(event)
+					callback = function(event2)
 						vim.lsp.buf.clear_references()
-						vim.api.nvim_clear_autocmds({ group = "lsp-highlight", buffer = event.buf })
+						vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
 					end,
 				})
 			end
 
-			-- Inlay hints
-			if client and client.server_capabilities.inlayHintProvider then
+			if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, bufnr) then
 				map("<leader>th", function()
 					vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr }))
 				end, "[T]oggle Inlay [H]ints")
 			end
+
+			if client and client:supports_method("textDocument/completion") then
+				vim.lsp.completion.enable(true, client.id, bufnr)
+			end
 		end
 
-		-- Diagnostic configuration
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+			callback = function(event)
+				local client = vim.lsp.get_client_by_id(event.data.client_id)
+				if client then
+					on_attach(client, event.buf)
+				end
+			end,
+		})
+
 		vim.diagnostic.config({
 			severity_sort = true,
-			float = {
-				border = "rounded",
-				source = "always",
-				focusable = false,
-				style = "minimal",
-				header = "",
-				prefix = "",
-			},
+			float = { border = "rounded", source = "if_many" },
 			underline = { severity = vim.diagnostic.severity.ERROR },
 			signs = {
 				text = {
@@ -116,34 +125,48 @@ return {
 			},
 			virtual_text = {
 				source = "if_many",
-				spacing = 4,
-				prefix = "●",
+				spacing = 2,
+				format = function(diagnostic)
+					local diagnostic_message = {
+						[vim.diagnostic.severity.ERROR] = diagnostic.message,
+						[vim.diagnostic.severity.WARN] = diagnostic.message,
+						[vim.diagnostic.severity.INFO] = diagnostic.message,
+						[vim.diagnostic.severity.HINT] = diagnostic.message,
+					}
+					return diagnostic_message[diagnostic.severity]
+				end,
 			},
 		})
 
-		-- Base capabilities
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
 
-		-- Server configurations
 		local servers = {
-			bashls = { on_attach = on_attach, flags = lsp_flags, capabilities = capabilities },
-			marksman = { on_attach = on_attach, flags = lsp_flags, capabilities = capabilities },
+			bashls = {
+				on_attach = on_attach,
+				flags = lsp_flags,
+				capabilities = capabilities,
+			},
+			marksman = {
+				on_attach = on_attach,
+				flags = lsp_flags,
+				capabilities = capabilities,
+			},
 			clangd = {
 				cmd = { "clangd" },
 				filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
 				single_file_support = true,
 				on_attach = on_attach,
-				capabilities = vim.tbl_deep_extend("force", capabilities, { offsetEncoding = "utf-8" }),
+				capabilities = capabilities,
 				flags = lsp_flags,
-				root_dir = require("lspconfig").util.root_pattern(
+				root_markers = {
 					".clangd",
 					".clang-tidy",
 					".clang-format",
 					"compile_commands.json",
 					"compile_flags.txt",
 					"configure.ac",
-					".git"
-				),
+					".git",
+				},
 				init_options = { fallbackFlags = { "-std=c++2a" } },
 			},
 			basedpyright = {
@@ -162,7 +185,12 @@ return {
 					},
 				},
 			},
-			jinja_lsp = { on_attach = on_attach, flags = lsp_flags, capabilities = capabilities },
+
+			jinja_lsp = {
+				on_attach = on_attach,
+				flags = lsp_flags,
+				capabilities = capabilities,
+			},
 			lua_ls = {
 				on_attach = on_attach,
 				flags = lsp_flags,
@@ -174,11 +202,14 @@ return {
 							library = vim.api.nvim_get_runtime_file("", true),
 							checkThirdParty = false,
 						},
-						telemetry = { enable = false },
+						telemetry = { enable = false }, -- Added to disable telemetry
 					},
 				},
 			},
 			emmet_ls = {
+				on_attach = on_attach,
+				capabilities = capabilities,
+				flags = lsp_flags,
 				cmd = { "emmet-ls", "--stdio" },
 				filetypes = {
 					"astro",
@@ -196,15 +227,12 @@ return {
 					"vue",
 					"htmlangular",
 				},
-				on_attach = on_attach,
-				flags = lsp_flags,
-				capabilities = capabilities,
 			},
 			ts_ls = {
 				on_attach = on_attach,
 				flags = lsp_flags,
 				capabilities = capabilities,
-				settings = {
+				settings = { -- Added inlay hints
 					typescript = {
 						inlayHints = {
 							includeInlayParameterNameHints = "all",
@@ -220,44 +248,36 @@ return {
 			},
 			html = {
 				on_attach = on_attach,
-				flags = lsp_flags,
 				capabilities = capabilities,
-				provideFormatter = true,
-				filetypes = { "html", "htmldjango" },
+				flags = lsp_flags,
+				provideFormatter = true, -- Added for built-in formatting
+				filetypes = { "html", "htmldjango" }, -- Explicitly include htmldjango
 			},
 			cmake = {
 				cmd = { "cmake-language-server" },
 				filetypes = { "cmake" },
 				init_options = { buildDirectory = "build" },
-				root_dir = require("lspconfig").util.root_pattern(
+				root_markers = {
 					"CMakePresets.json",
 					"CTestConfig.cmake",
 					".git",
 					"build",
-					"cmake"
-				),
+					"cmake",
+				},
 				single_file_support = true,
 				on_attach = on_attach,
 				flags = lsp_flags,
 				capabilities = capabilities,
 			},
-			gopls = {
-				cmd = { "gopls" },
-				filetypes = { "go", "gomod", "gowork", "gotmpl" },
-				root_dir = require("lspconfig").util.root_pattern("go.mod", ".git"),
-				single_file_support = true,
-				on_attach = on_attach,
-				flags = lsp_flags,
-				capabilities = capabilities,
-			},
+			-- End of server configs
 		}
 
-		-- Mason tool installer setup
 		local ensure_installed = vim.tbl_keys(servers or {})
-		vim.list_extend(ensure_installed, { "stylua", "prettierd", "clang-format" })
+		vim.list_extend(ensure_installed, {
+			"stylua",
+			"prettierd",
+		})
 		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
-
-		-- Mason and LSP config setup
 		require("mason").setup({})
 		require("mason-lspconfig").setup({
 			ensure_installed = {},
@@ -271,16 +291,8 @@ return {
 			},
 		})
 
-		-- Lspsaga setup (for file header feature)
-		require("lspsaga").setup({
-			server_filetype_map = { typescript = "typescript" },
-			scroll_preview = { scroll_down = "<C-f>", scroll_up = "<C-b>" },
-			definition = { edit = "<CR>" },
-			ui = {
-				winblend = 10,
-				border = "rounded",
-				colors = { normal_bg = "#002b36" },
-			},
-		})
+		-- vim.opt.completeopt = { "menuone", "noselect", "popup" }
+		-- vim.keymap.set("i", "<c-space>", vim.lsp.completion.get)
+		-- vim.keymap.set("i", "<cr>", "pumvisible() ? '<c-y>' : '<cr>'", { expr = true })
 	end,
 }
