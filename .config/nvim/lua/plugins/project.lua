f2eece81e04642e60de24d7257db6e8377288ed5
appyouz <dikpalkhatri7@gmail.com>
2025-04-30 09:52:22 +0545

    Refactor: neovim-project

--- .config/nvim/lua/plugins/project.lua
+++ .config/nvim/lua/plugins/project.lua
@@ -1,41 +1,23 @@
 return {
-	"ahmedkhalf/project.nvim",
-	init = function()
-		require("project_nvim").setup({
-			-- your configuration comes her
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
-		})
-
-		-- Absolutely minimal implementation of fzf-lua based project finder
-		-- for fzf-lua, due to request from @KrisWilliams1 (Maybe extended to a
-		-- full blown port from the original selector in the future)
-		local history = require("project_nvim.utils.history")
-		local project = require("project_nvim.project")
-
-		vim.api.nvim_create_user_command("FzfProjects", function()
-			local projects = history.get_recent_projects()
-
-			require("fzf-lua").fzf_exec(projects, {
-				prompt = "Projects> ",
-				actions = {
-					["default"] = function(selected)
-						if selected and #selected > 0 then
-							local project_path = selected[1]
-							if project.set_pwd(project_path, "fzf-lua") then
-								require("fzf-lua").files()
-							end
-						end
-					end,
-				},
-			})
-		end, {})
-	end,
-	keys = {
-		{
-			"<leader>fp",
-			"<cmd>FzfProjects<CR>",
-			desc = "Find Recent Projects",
+	"coffebar/neovim-project",
+	opts = {
+		projects = { -- define project roots
+			"~/projects/*",
+			"~/.config/*",
+		},
+		picker = {
+			type = "fzf-lua", -- or "fzf-lua"
 		},
 	},
+	init = function()
+		-- enable saving the state of plugins in the session
+		vim.opt.sessionoptions:append("globals") -- save global variables that start with an uppercase letter and contain at least one lowercase letter.
+	end,
+	dependencies = {
+		{ "nvim-lua/plenary.nvim" },
+		{ "ibhagwan/fzf-lua" },
+		{ "Shatur/neovim-session-manager" },
+	},
+	lazy = false,
+	priority = 100,
 }
